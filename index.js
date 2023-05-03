8a161400d1b8772f288c64178b5635edd9cd355f
Gabe Soelberg <120156274+gabesoelberg@users.noreply.github.com>
2023-05-02 20:08:26 -0600

    Updated Game

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 const options = ["rock", "paper", "scissors"];
+const images = document.querySelectorAll(".game img");
+const messageDiv = document.getElementById("message");
+const scoreDiv = document.getElementById("score");
+const resetButton = document.getElementById("reset");
 
-let playerChoice = prompt("Choose rock, paper, or scissors").toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
 
-while (!options.includes(playerChoice.toLowerCase())) {
-    playerChoice = prompt("Invalid choice. Choose rock, paper, or scissors");
-}
+images.forEach(img => {
+    img.addEventListener("click", function() {
+        let playerChoice = this.dataset.choice;
 
-const computerChoice = options[Math.floor(Math.random() * options.length)];
+        const computerChoice = options[Math.floor(Math.random() * options.length)];
 
-let result; 
+        let result;
 
-if (playerChoice === computerChoice) {
-    result = "It's a tie!";
-} else if (
-    (playerChoice === "rock" && computerChoice === "scissors") ||
-    (playerChoice === "paper" && computerChoice === "rock") ||
-    (playerChoice === "scissors" && computerChoice === "paper")
-) {
-    result = "You win!";
-} else {
-    result = "You lose!";
-}
+        if (playerChoice === computerChoice) {
+            result = "It's a tie!";
+        } else if (
+            (playerChoice === "rock" && computerChoice === "scissors") ||
+            (playerChoice === "paper" && computerChoice === "rock") ||
+            (playerChoice === "scissors" && computerChoice === "paper")
+        ) {
+            result = "You win!";
+            playerScore++;
+        } else {
+            result = "You lose!";
+            computerScore++;
+        }
 
-alert(`You chose ${playerChoice}. The computer chose ${computerChoice}. ${result}`)
+        messageDiv.textContent = `You chose ${playerChoice}. The computer chose ${computerChoice}. ${result}`;
+        scoreDiv.textContent = `Your score: ${playerScore}, Computer's score: ${computerScore}`;
+
+        if (playerScore === 5 || computerScore === 5) {
+            let finalResult = playerScore > computerScore ? "Congratulations, you won the series!" : "Sorry, you lost the series.";
+            messageDiv.textContent = `${finalResult} Your score: ${playerScore}, Computer's score: ${computerScore}`;
+            resetButton.style.display = "block";
+        }
+    });
+});
+
+resetButton.addEventListener("click", function() {
+    playerScore = 0;
+    computerScore = 0;
+    messageDiv.textContent = "";
+    scoreDiv.textContent = "";
+    resetButton.style.display = "none";
+});
+
